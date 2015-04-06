5ca3f90bc425f1117e54197a595c7d2aa207bdb9
sunny chao <sunny.chao@makerbot.com>
2015-04-06 15:39:35 -0400

    carriage animation

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	function gantryUp(){
+		var up = new TimelineLite();
+		up.to($('.gantry'), 2, {marginTop:"-30px", ease:Linear.easeNone, delay:.3})
+			.to($('.carriage'), 1.5, {marginLeft:"55px", ease:Linear.easeNone})
+			.to($('.carriage'), 2.5, {marginLeft:"176px", ease:Linear.easeNone, onComplete:carriageMove});
+	};
+	function carriageMove(){
+		var move = new TimelineLite();
+		move.to($('.carriage'), 2.5, {marginLeft:"55px", ease:Linear.easeNone})
+			.to($('.carriage'), 2.5, {marginLeft:"176px", ease:Linear.easeNone, onComplete:carriageMove});
+	}
+	gantryUp();
+	// carriageMove();
+});
