2b908874ef67043505010882790a3ad678c8f235
sunny chao <sunny.chao@makerbot.com>
2015-04-06 17:05:15 -0400

    Knob LED

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	function gantryUp(){
 		var up = new TimelineLite();
-		up.to($('.gantry'), 2, {marginTop:"-30px", ease:Linear.easeNone, delay:.3})
+		up.to($('.knobLed'), .3, {opacity:"1", ease:Linear.easeNone, delay:.3})
+			.to($('.screen'), .2, {opacity:"1", ease:Linear.easeNone})
+			.to($('.gantry'), 2, {marginTop:"-30px", ease:Linear.easeNone,delay:.4})
 			.to($('.carriage'), 1.5, {marginLeft:"55px", ease:Linear.easeNone, delay:.2})
 			.to($('.carriage'), 2.5, {marginLeft:"176px", ease:Linear.easeNone, onComplete:carriageMove});
 	};
