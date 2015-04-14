a1a4bae4a3e0e082526db403e7e1ccec2ada8698
sunny chao <sunny.chao@makerbot.com>
2015-04-14 15:48:20 -0400

    testing forms

--- js/script.js
+++ js/script.js
@@ -60,4 +60,30 @@ $(document).ready(function(){
 	}).mousemove(function(e){
 		e.preventDefault()
 	});
+
+
+	var selDiv = "";
+		
+	document.addEventListener("DOMContentLoaded", init, false);
+	
+	function init() {
+		document.querySelector('#files').addEventListener('change', handleFileSelect, false);
+		selDiv = document.querySelector("#selectedFiles");
+	}
+		
+	function handleFileSelect(e) {
+		
+		if(!e.target.files) return;
+		
+		selDiv.innerHTML = "";
+		
+		var files = e.target.files;
+		for(var i=0; i<files.length; i++) {
+			var f = files[i];
+			
+			selDiv.innerHTML += f.name + "<br/>";
+
+		}
+		
+	}
 });
