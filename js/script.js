0b50ad8c41efb5d86c10cdc2f8692db6768225a8
sunny chao <sunny.chao@makerbot.com>
2015-04-15 16:29:11 -0400

    rebuilt task page

--- js/script.js
+++ js/script.js
@@ -62,28 +62,20 @@ $(document).ready(function(){
 	});
 
 
-	var selDiv = "";
-		
-	document.addEventListener("DOMContentLoaded", init, false);
-	
-	function init() {
-		document.querySelector('#files').addEventListener('change', handleFileSelect, false);
-		selDiv = document.querySelector("#selectedFiles");
-	}
-		
-	function handleFileSelect(e) {
-		
-		if(!e.target.files) return;
-		
-		selDiv.innerHTML = "";
-		
-		var files = e.target.files;
-		for(var i=0; i<files.length; i++) {
-			var f = files[i];
-			
-			selDiv.innerHTML += f.name + "<br/>";
+	//add category button
+	$(".addCat").on('click', function(){
+		$('.catList').append("<option>" + $(".catInput").val() + "</option>");
+		$('.catInput').val("");
+	})
 
-		}
-		
-	}
+	//add subcategory button
+	$(".addsubCat").on('click', function(){
+		$('.subcatList').append("<option>" + $(".subcatInput").val() + "</option>");
+		$('.subcatInput').val("");
+	})
+
+
+
+
+
 });
